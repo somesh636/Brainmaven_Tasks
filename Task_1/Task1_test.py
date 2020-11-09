92072f51d99aac384c4955d71ae6d78d19d95c5e
Somesh <somesh110296@gmail.com>
2020-11-08 22:01:21 -0500

    added comments in the Test file

--- Task_1/Task1_test.py
+++ Task_1/Task1_test.py
@@ -4,9 +4,11 @@ import unittest
 class TestLRU(unittest.TestCase): 
 
     def test_LRU_Class(self): 
+        """ Test Case: To check for negative value initialization for the LRU class """
         self.assertRaises(ValueError, LRU, -1)
 
     def test_put_1(self): 
+        """ Test Case: To check for put functionality for one value """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -16,6 +18,7 @@ class TestLRU(unittest.TestCase):
         self.assertEqual(lru.get("5"), 5, "should be 5")
 
     def test_put_2(self): 
+        """ Test Case: To check for multiple put functionality after LRU maximum size is achieved """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -29,6 +32,7 @@ class TestLRU(unittest.TestCase):
         self.assertEqual(lru.get("7"), 7, "should be 7")
 
     def test_put_3(self): 
+        """ Test Case: To check for put functionality for the value which is not the most recent in the size of LRU """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -44,6 +48,7 @@ class TestLRU(unittest.TestCase):
         
    
     def test_get_1(self): 
+        """ Test Case: To check for get functionality for all the values which was put using put function """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -57,6 +62,7 @@ class TestLRU(unittest.TestCase):
         self.assertEqual(lru.get("5"), 5, "should be 5")
 
     def test_get_2(self): 
+        """ Test Case: To check for get functionality for a value that doesn't exist in the LRU cache """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -71,6 +77,7 @@ class TestLRU(unittest.TestCase):
         self.assertRaises(KeyError, lru.get, "10")
 
     def test_get_3(self): 
+        """ Test Case: To check for get functionality after multiple put """
         lru = LRU(15)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -96,6 +103,7 @@ class TestLRU(unittest.TestCase):
 
 
     def test_delete_1(self): 
+        """ Test Case: To check for delete functionality for different scnearios """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -113,6 +121,7 @@ class TestLRU(unittest.TestCase):
         self.assertEqual(lru.get("2"), 2, "should be 2")
 
     def test_delete_2(self): 
+        """ Test Case: To check for delete functionality for one value """
         lru = LRU(15)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -124,6 +133,7 @@ class TestLRU(unittest.TestCase):
         self.assertEqual(lru.get("3"), 3, "should be 3")
 
     def test_delete_3(self): 
+        """ Test Case: To check for multiple delete functionality for one value """
         lru = LRU(1)
         lru.put("1", 1)
         lru.delete("1")
@@ -132,6 +142,7 @@ class TestLRU(unittest.TestCase):
         
 
     def test_size_1(self): 
+        """ Test Case: To check for size of LRU with put and get functionality """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -146,15 +157,16 @@ class TestLRU(unittest.TestCase):
         
         self.assertEqual(lru.get("10"), 10, "should be 10")
         self.assertRaises(KeyError, lru.get, "1")
-        self.assertEqual(lru.get("9"), 9, "should be 10")
+        self.assertEqual(lru.get("9"), 9, "should be 9")
         self.assertRaises(KeyError, lru.get, "2")
-        self.assertEqual(lru.get("8"), 8, "should be 10")
+        self.assertEqual(lru.get("8"), 8, "should be 8")
         self.assertRaises(KeyError, lru.get, "3")
-        self.assertEqual(lru.get("7"), 7, "should be 10")
+        self.assertEqual(lru.get("7"), 7, "should be 7")
         self.assertRaises(KeyError, lru.get, "5")
-        self.assertEqual(lru.get("6"), 6, "should be 10")
+        self.assertEqual(lru.get("6"), 6, "should be 6")
 
     def test_size_2(self): 
+        """ Test Case: To check for size of LRU with multiple put and get functionality with different size initialization """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -202,6 +214,7 @@ class TestLRU(unittest.TestCase):
 
 
     def test_reset_1(self): 
+        """ Test Case: To check the reset funcitonality for the LRU algorithm """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -222,6 +235,7 @@ class TestLRU(unittest.TestCase):
         self.assertRaises(KeyError, lru.get, "9")
 
     def test_reset_2(self): 
+        """ Test Case: To check for reset functionality of LRU with put and get functionality """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
@@ -238,6 +252,7 @@ class TestLRU(unittest.TestCase):
         self.assertRaises(KeyError, lru.get, "10")
         
     def test_reset_3(self): 
+        """ Test Case: To check for reset functionality of LRU put and get functionality after using reset """
         lru = LRU(5)
         lru.put("1", 1)
         lru.put("2", 2)
