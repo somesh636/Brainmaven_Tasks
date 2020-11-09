f8d2d8375ce324edb6b816f6cca578692349af6d
Somesh <somesh110296@gmail.com>
2020-11-08 21:26:51 -0500

    Task1 code for LRU algorithm

--- Task_1/Task1.py
+++ Task_1/Task1.py
@@ -5,7 +5,7 @@ class LRU(OrderedDict):
 
 	Parameters: 
 	-----------
-	max_size: int(default: 10)
+	max_size: int(default: 5)
 	          Maximum size requierd for the implementation
 		
 	
@@ -15,7 +15,7 @@ class LRU(OrderedDict):
     """ 
 
     def __init__(self, max_size=5): 
-        """ Initialization Fucntion for the clas  """
+        """ Initialization Fucntion for the class  """
         if max_size<0: 
             raise ValueError 
 
@@ -78,29 +78,66 @@ class LRU(OrderedDict):
         except KeyError: 
             raise KeyError
 
+    def reset(self):
+        """ Deletes all the key values from the LRU 
+        Parameters: 
+        -----------
+        None   
+        
+        Attributes:
+        -----------
+        None 
+        """
+        self.clear()
+
+
 if __name__ == "__main__": 
 
     # Exploratory Testing 
-    lru = LRU(5)
-    lru.put("5", "sg")
-    lru.put("6", "pg")
-    lru.put("7", "s")
-    lru.put("8", "p")
-    lru.put("9", "so")
-    lru.put("10", "pr")
-    lru.put("11", "som")
-    lru.put("12", "pri")
-    lru.delete("10")
-    value = lru.get("12")
-    print(value) 
-    value1 = lru.get("7")
-    print(value1)  
-    value2 = lru.get("100")
-    print(value2) 
-    value3 = lru.get("5")
-    print(value3) 
-    value4 = lru.get("12")
-    print(value4)
+    lru = LRU(15)
+    lru.put("11", "sg")
+    lru.put("12", "pg")
+    lru.put("13", "s")
+    lru.put("14", "p")
+    lru.put("15", "so")
+    lru.put("16", "pr")
+    lru.put("17", "som")
+    lru.put("18", "pri")
+    lru.put("1", 1)
+    lru.put("2", 2)
+    lru.put("3", 3)
+    lru.put("4", 4)
+    lru.put("5", 5)
+    lru.put("6", 6)
+    lru.put("7", 7)
+    lru.put("8", 8)
+    lru.put("9", 9)
+    lru.put("10", 10)
+
+    value_1 = lru.get("1")
+    value_2 = lru.get("2")
+    value_3 = lru.get("3")
+    value_4 = lru.get("4")
+    value_5 = lru.get("5")
+    value_6 = lru.get("6")
+    value_7 = lru.get("7")
+    value_8 = lru.get("8")
+    value_9 = lru.get("9")
+    value_10 = lru.get("10")
+    
+    print(value_1)
+    print(value_2)
+    print(value_3)
+    print(value_4)
+    print(value_5)
+    print(value_6)
+    print(value_7) 
+    print(value_8)
+    print(value_9)
+    print(value_10)
+
+
+
     
     
 
